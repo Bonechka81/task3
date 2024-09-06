ea528023f15e677e4a0a60b2db84809f49142d5b
Bonechka81 <asanova81.06@mail.ru>
2024-09-06 16:38:10 +0300

    Add files via upload

--- task3.py
+++ task3.py
@@ -1,5 +1,6 @@
-name='Python,'
 homework_count=12
 hour_count=1.5
-time_for_task=(1.5/12)
+name='Python,'
+time_for_task=(hour_count/homework_count)
+print(hour_count/homework_count)
 print('Курс:', name, 'всего задач:', homework_count,',','затрачено часов:', hour_count,',' 'среднее время выполнения:', time_for_task, 'часа' )
